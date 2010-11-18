3337f2efdff2e611b30d2019fe808be1e1014c17
Aliaksandr Zahatski <github.send@zag.ru>
2010-11-18 16:27:39 +0300

    clean depreacted deps

--- lib/Perl6/Pod/To/DocBook.pm
+++ lib/Perl6/Pod/To/DocBook.pm
@@ -65,7 +65,6 @@ use Perl6::Pod::Parser::AddHeadLevels;
 use Perl6::Pod::To::DocBook::ProcessHeads;
 use Perl6::Pod::Parser::ListLevels;
 use Perl6::Pod::Parser::Doformatted;
-use Perl6::Pod::Parser::Doallow;
 use XML::ExtOn('create_pipe');
 use base qw/Perl6::Pod::To::XML/;
 use constant POD_URI => 'http://perlcabal.org/syn/S26.html';
@@ -78,7 +77,6 @@ sub new {
       create_pipe( 'Perl6::Pod::To::DocBook::ProcessHeads', $self->{out_put} );
     return create_pipe(
         'Perl6::Pod::Parser::Doformatted',
-        'Perl6::Pod::Parser::Doallow',
         'Perl6::Pod::Parser::ListLevels',
         'Perl6::Pod::Parser::AddHeadLevels',
         'Test::FilterDocBook', $self
